6b649597c5bc00b3aa3490aaa25567ef0060e837
subhashish <someone@someplace.com>
2020-07-15 18:22:55 +0530

    how to make individual subarrays

---
+++ sub_array.py
@@ -0,0 +1,18 @@
+def sub_segments (string, n):
+    l = len (string)
+    for x in range (0, l, n):
+        newlist = string[x : x + n]
+
+        # New array for every iteration
+        arr = []
+        for y in newlist:
+
+           # Check if the character is in the array
+            if y not in arr:
+                arr.append (y)
+        print (''.join(arr))
+
+# Driver code
+string = "geeksforgeeksgfg"
+n = 4
+sub_segments (string, n)
